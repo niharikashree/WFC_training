90daa3331f1e4c9defa8810b94a982a3d0542eb2
niharikashree <167109338+niharikashree@users.noreply.github.com>
2025-07-30 15:07:40 +0530

    Create Largest Of Three using ternary operators

---
+++ LargestOfThree.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class LargestOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        int largest = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+        System.out.println("Largest: " + largest);
+    }
+}
