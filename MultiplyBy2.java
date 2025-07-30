9b8c000d8750e4d1101433711a90e88a3e1de40f
niharikashree <167109338+niharikashree@users.noreply.github.com>
2025-07-30 15:15:28 +0530

    Create multiply without using *

---
+++ MultiplyBy2.java
@@ -0,0 +1,7 @@
+public class MultiplyBy2 {
+    public static void main(String[] args) {
+        int num = 7;
+        int result = num << 1;
+        System.out.println("Multiplied by 2: " + result);
+    }
+}
