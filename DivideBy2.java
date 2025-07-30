34a26910a4db6e8e0c4633aff1a6eecb7698949a
niharikashree <167109338+niharikashree@users.noreply.github.com>
2025-07-30 15:16:07 +0530

    Create Divide Without Using /

---
+++ DivideBy2.java
@@ -0,0 +1,7 @@
+public class DivideBy2 {
+    public static void main(String[] args) {
+        int num = 10;
+        int result = num >> 1;
+        System.out.println("Divided by 2: " + result);
+    }
+}
