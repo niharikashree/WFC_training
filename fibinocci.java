c2c7cae5c9d69c14936a61a09222b12559808b66
niharikashree <167109338+niharikashree@users.noreply.github.com>
2025-07-30 15:00:56 +0530

    Create fibinocci.java

---
+++ fibinocci.java
@@ -0,0 +1,16 @@
+ublic class fibinocci {
+    public static void main(String[] args) {
+        int n = 10; // Change this value to generate more Fibonacci numbers
+        System.out.println("Fibonacci series up to " + n + " terms:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int num) {
+        if (num <= 1) {
+            return num;
+        }
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+} 
