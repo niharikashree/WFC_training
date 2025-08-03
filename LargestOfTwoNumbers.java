6ef4eb1934a0019561c5dd5da88041b577865059
niharikashree <167109338+niharikashree@users.noreply.github.com>
2025-08-03 16:21:02 +0530

    Create Largest Of Two Numbers

---
+++ LargestOfTwoNumbers.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class LargestOfTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        int max = (a > b) ? a : b;
+        System.out.println("Largest: " + max);
+    }
+}
